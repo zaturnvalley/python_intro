391ae9fb4f86735d92f412ca9a4824b50f395920
zaturnvalley <zacharycmason@gmail.com>
2016-11-01 11:48:14 -0700

    added math and variables section

--- hi.py
+++ hi.py
@@ -1,4 +1,24 @@
+#python print and strings
 print('hi')
 print('good to meet','you')
 print(5+4)
 print('my favorite number is', 10)
+
+#python math
+print(2/3) #new to python 3 prints .33 
+print(2*3)
+print(1.5/2.6) #floats
+print(4**2) #4 to the power of 2
+
+#python variables
+exVar = 100
+print(exVar)
+opVar = exVar / 5
+print(opVar)
+#cannot use numbers at beginning, so use underscore before, ie:
+_100ma = 5
+print(_100ma)
+
+#seems super similar to ruby so far, except print instead of puts
+
+#while loops
