a4ddb843fa386da7e869c27ddaebf2a7adb2f57d
zaturnvalley <zacharycmason@gmail.com>
2016-11-02 20:40:50 -0700

    python functions

--- basicsPartTwo.py
+++ basicsPartTwo.py
@@ -6,15 +6,30 @@ def addition(num1,num2,num3,num4):
 x = addition(1,2,3,4)
 print(x)
 
-def website(font, background_color, font_size, font_color):
+# def website(font, background_color, font_size, font_color):
+#   print('font:', font)
+#   print('bg:', background_color)
+#   print('font Size:', font_size)
+#   print('font color:', font_color)
+
+# website('Futura','White','11','Black')
+# #can do out of order
+# website(font_color='black',
+#         font='TNR',
+#         font_size='11',
+#         background_color='black')
+
+#can customize defaults this way
+def website(font='TNR', 
+            background_color='black', 
+            font_size=11, 
+            font_color='white'):
   print('font:', font)
   print('bg:', background_color)
   print('font Size:', font_size)
   print('font color:', font_color)
 
-website('Futura','White','11','Black')
-#can do out of order
-website(font_color='black',
-        font='TNR',
-        font_size='11',
-        background_color='black')
+#then can change those defaults via this
+website(background_color = 'green')
+
+#global global and local variables
