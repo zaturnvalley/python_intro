bc4b6622f1ebc6e4f973841384c9138d95c8de26
zaturnvalley <zacharycmason@gmail.com>
2016-11-06 17:31:31 -0800

    finshed err section

--- basicsPartThree.py
+++ basicsPartThree.py
@@ -7,7 +7,7 @@ exampleModule.exampleFunc('test')
 #third part modules are in packages
 #needs to be in local dir of proj yr working on in lib
 
-#error handling
+#error handling with try and accept
 try:
   print('Running the try...')
   import mars #this will throw import error
@@ -15,15 +15,20 @@ try:
 
   #will say type error cannot print int to string
 except Exception as e:
-  pring(str(e))
   print('General Exception')
+  pring(str(e))
 
 except TypeError as t:
   print('TypeError Triggered')
+  pring(str(t))
 
 except NameError as n:
   print('NameError Triggered')
-
-except Exception as e:
+  pring(str(n))
 
 print('Code continues onward...')
+
+# try:
+#   name = input('what is your name')
+#   print(str(name))
+
