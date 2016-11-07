451e64889a42d2aece038fc6404a9bb258797029
zaturnvalley <zacharycmason@gmail.com>
2016-11-06 17:22:58 -0800

    error handling section

--- basicsPartThree.py
+++ basicsPartThree.py
@@ -8,3 +8,22 @@ exampleModule.exampleFunc('test')
 #needs to be in local dir of proj yr working on in lib
 
 #error handling
+try:
+  print('Running the try...')
+  import mars #this will throw import error
+  print('5'+5)
+
+  #will say type error cannot print int to string
+except Exception as e:
+  pring(str(e))
+  print('General Exception')
+
+except TypeError as t:
+  print('TypeError Triggered')
+
+except NameError as n:
+  print('NameError Triggered')
+
+except Exception as e:
+
+print('Code continues onward...')
