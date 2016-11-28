ef470e782046659b907c53c86910d1d16e4fd92f
zaturnvalley <zacharycmason@gmail.com>
2016-11-28 13:36:04 -0800

    action input added

--- teacherProgram.py
+++ teacherProgram.py
@@ -1,9 +1,13 @@
 # Program For Keeping a Log of Student Grades
 def main():
   print("""
-  Welcome to Grade Centrl
+  Welcome to Grade Central
+
   [1] - Enter Grades
   [2] - Remove Student
   [3] - Student Average Grades
   [4] - Exit
   """)
+  action = input('What would you like to do today? (Enter a number ')
+
+main()
