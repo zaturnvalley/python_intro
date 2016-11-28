bdc51c8e0cedb3491ea1ac687d5d6d66819a0310
zaturnvalley <zacharycmason@gmail.com>
2016-11-28 13:34:41 -0800

    added start of teacher program

---
+++ teacherProgram.py
@@ -0,0 +1,9 @@
+# Program For Keeping a Log of Student Grades
+def main():
+  print("""
+  Welcome to Grade Centrl
+  [1] - Enter Grades
+  [2] - Remove Student
+  [3] - Student Average Grades
+  [4] - Exit
+  """)
