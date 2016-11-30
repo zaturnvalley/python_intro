fcedaa5cf3ae380ddafa95a4809c542d62886aa8
zaturnvalley <zacharycmason@gmail.com>
2016-11-29 22:26:07 -0800

    added upgrade method

--- oopInPython.py
+++ oopInPython.py
@@ -6,5 +6,11 @@ class Program():
     self.lang = input("What Language?: ")
     self.version = float(input("Version?: "))
     self.skill = input("What skill level?: ")
-    
+  
+  def upgrade(self):
+    new_version = input("What version?: ")
+    print("We have updated the version for", self.lang) 
+    self.version = new_version
+
 p1 = Program()
+# p2 = Program()
