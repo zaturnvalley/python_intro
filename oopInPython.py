b9bf7db2b9f3a4a92a2cf277f4d323592e56b59a
zaturnvalley <zacharycmason@gmail.com>
2016-11-29 22:12:26 -0800

    added skilla nd version, class set to p1

--- oopInPython.py
+++ oopInPython.py
@@ -2,5 +2,9 @@ class Program():
   # init method is a special method that
   # always runs, default
   def __init__(self, *args, **kwargs)
-    # modifying language itself
+    # modifying language itself, stored in init
     self.lang = input("What Language?: ")
+    self.version = float(input("Version?: "))
+    self.skill = input("What skill level?: ")
+    
+p1 = Program()
