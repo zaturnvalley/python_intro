b7e129e40720ab55284ec0f407c589ffa19bbe4f
zaturnvalley <zacharycmason@gmail.com>
2016-11-29 22:10:29 -0800

    created oopInPython

---
+++ oopInPython.py
@@ -0,0 +1,6 @@
+class Program():
+  # init method is a special method that
+  # always runs, default
+  def __init__(self, *args, **kwargs)
+    # modifying language itself
+    self.lang = input("What Language?: ")
